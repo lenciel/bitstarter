6e07748abe3193a538cb8cd9c192e97ecaaa9a59
vagrant <vagrant@ubuntu-server-12042-x64-vbox4210.(none)>
2013-07-09 06:57:12 +0000

    read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+var input_file = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile(input_file, function (err, data) {
+    if (err) throw err;
+    response.send(data.toString('utf-8'));
+  });
 });
 
 var port = process.env.PORT || 5000;
